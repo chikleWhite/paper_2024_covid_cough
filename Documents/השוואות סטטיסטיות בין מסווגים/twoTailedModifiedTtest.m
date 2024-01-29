2cb80d3c49896a5fdf6b74b5cecff972e4808db6
Nir Iakoby <136971404+chikleWhite@users.noreply.github.com>
2024-01-29 12:49:38 +0200

    Add files via upload

---
+++ Documents/השוואות סטטיסטיות בין מסווגים/twoTailedModifiedTtest.m
@@ -0,0 +1,23 @@
+function pValue = twoTailedModifiedTtest(PerformanceClassifierA, PerformanceClassifierB, numSubjTrainVal, numsubjTest)
+
+% corrected resampled two tailed t-test based on Nadeau and Bengio.
+% ref: https://link.springer.com/content/pdf/10.1023/A:1024068626366.pdf
+
+n = length(PerformanceClassifierA); % No. iterations
+
+% difference between two models results
+difference = PerformanceClassifierA - PerformanceClassifierB;
+differenceMean = sum(difference) / n; % mean
+differenceVar = (difference - differenceMean)' * (difference - differenceMean) / (n - 1); % variance
+
+% variance after correction
+differenceVarModified = differenceVar * (1/n + numsubjTest / numSubjTrainVal);
+
+% t value
+t = differenceMean / sqrt(differenceVarModified);
+
+% Probability of larger t-statistic (two tailed)
+pValue = (1 - tcdf(abs(t), n-1)) * 2;
+
+end
+
